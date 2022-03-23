c6278d60683799a8d239df4f590fbde65512a6ce
Mohd Aminuddin <68073525+mhdamn34@users.noreply.github.com>
2022-03-23 15:12:29 +0800

    push create

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -3,53 +3,46 @@
 @section('content')
 
 <div class="card">
+
     <div class="card-header">
-        <form action=" {{ route('customer.store') }} " method="POST">
-            <h5>Untitled</h5>
-
-            <div class="row">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#" id="gotoId">Go To <span class="sr-only"></span></a>
-                            <li class="nav-item active">
-                                <div class="input-group mb-3">
-                                    <select class="form-select" id="gotoId">
-                                        <option selected>Choose...</option>
-                                        <option value="1">One</option>
-                                        <option value="2">Two</option>
-                                        <option value="3">Three</option>
-                                    </select>
-                                </div>
-                            </li>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">E-mail Customer</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Create Outlook Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Save and New</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href=" {{ url('/customer') }} ">Close</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-
-
-
-            </div>
+        <h5>Untitled</h5>
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#" id="gotoId">Go To <span class="sr-only"></span></a>
+                        <li class="nav-item active">
+                            <div class="input-group mb-3">
+                                <select class="form-select" id="gotoId">
+                                    <option selected>Choose...</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">E-mail Customer</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Create Outlook Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Save and New</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href=" {{ url('/customer') }} ">Close</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </div>
 
-
     <div class="card-body">
-
         @if ($errors->any())
-
         <div class="alert alert-danger">
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
             <ul>
@@ -58,46 +51,37 @@
                 @endforeach
             </ul>
         </div>
-
         @endif
-
-
-
-        @csrf
-        <div class="row">
-            <div class="col">
-                <form>
+        <form action=" {{ route('customer.store') }} " method="POST">
+            @csrf
+            <div class="row">
+                <div class="col">
                     <div class="row mb-3">
                         <label for="company" class="col-sm-3 col-form-label">Company</label>
                         <div class="col-sm-9">
                             <input type="text" name="company" class="form-control" id="company">
                         </div>
                     </div>
-
                     <br>
                     <h5>Primary Contact</h5>
-
                     <div class="row mb-3">
                         <label for="firstname" class="col-sm-3 col-form-label">First Name</label>
                         <div class="col-sm-9">
                             <input type="text" name="first_name" class="form-control" id="firstname">
                         </div>
                     </div>
-
                     <div class="row mb-3">
                         <label for="lastname" class="col-sm-3 col-form-label">Last Name</label>
                         <div class="col-sm-9">
                             <input type="text" name="last_name" class="form-control" id="lastname">
                         </div>
                     </div>
-
                     <div class="row mb-3">
                         <label for="job" class="col-sm-3 col-form-label">Job Title</label>
                         <div class="col-sm-9">
                             <input type="text" name="job_title" class="form-control" id="jobTitle">
                         </div>
                     </div>
-
                     <br>
                     <h5>Phone Numbers</h5>
                     <div class="row mb-3">
@@ -156,46 +140,39 @@
                             <input type="text" name="country" class="form-control" id="country">
                         </div>
                     </div>
+                </div>
+                <div class="col">
+                    <div class="row mb-3">
+                        <label for="email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="email_address" class="form-control" id="email">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="web" class="col-sm-2 col-form-label">Web Page</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="web_page" class="form-control" id="web">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="notes" class="col-sm-2 col-form-label">Notes</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="notes" class="form-control" id="notes">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="img" class="col-sm-2 col-form-label">Image</label>
+                        <div class="col-sm-10">
+                            <input type="file" class="form-control" id="img">
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col">
-                <div class="row mb-3">
-                    <label for="email" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="email_address" class="form-control" id="email">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="web" class="col-sm-2 col-form-label">Web Page</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="web_page" class="form-control" id="web">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="notes" class="col-sm-2 col-form-label">Notes</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="notes" class="form-control" id="notes">
-                    </div>
-                </div>
-                <div class="row mb-3">
-                    <label for="img" class="col-sm-2 col-form-label">Image</label>
-                    <div class="col-sm-10">
-                        <input type="file" class="form-control" id="img">
-                    </div>
-                </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary"> Save</button>
             </div>
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary"> Save</button>
-        </div>
         </form>
-
     </div>
 
-
 </div>
-
-
-
-
 @endsection
