77dfa71ba942c18ef4df8aae0c396aa99cca8957
Afrina16 <nurafrinaalimi16@gmail.com>
2022-03-29 08:27:33 +0800

    product view

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <div class="form-group">
                         <label>Product Code :</label>
-                        <p><b>{{ $product -> created_at }}</b></p>
+                        <p><b>{{ $product -> product_code }}</b></p>
                     </div>
                     <div class="form-group">
                         <label>Category :</label>
@@ -25,9 +25,30 @@
                         <label>Supplier :</label>
                         <p><b>{{ $product -> supplier_id }}</b></p>
                     </div>
+                    <div class="form-group">
+                        <label>Standard Cost :</label>
+                        <p><b>{{ $product -> standard_cost }}</b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>List Price :</label>
+                        <p><b>{{ $product -> list_price }}</b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Reorder Level :</label>
+                        <p><b>{{ $product -> reorder_level }}</b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Target Level :</label>
+                        <p><b>{{ $product -> target_level }}</b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Discontinued :</label>
+                        <p><b>{{ $product -> discontinued }}</b></p>
+                    </div>
+
                    
                     
-                    <p><a href="{{ route('products.index') }}" class="btn btn-primary">Back</a></p>
+                    <p><a href="{{ route('product.index') }}" class="btn btn-primary">Back</a></p>
                 </div>
             </div>        
         </div>
