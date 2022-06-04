93f2a2101aede39e1f62650043ec072105c2762f
Mohd Aminuddin <aminhamzah34@gmail.com>
2022-06-04 14:57:48 +0800

    redev

--- resources/views/layouts/main/app.blade.php
+++ resources/views/layouts/main/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,20 +12,21 @@
     @include('layouts.shared.style')
 
 </head>
-    <body class="hold-transition sidebar-mini">
-        <!-- Wrapper -->
-        <div id="app" class="wrapper">
 
-            <!-- sidebar -->
-            @yield('sidebar')
+<body class="hold-transition sidebar-mini">
+    <!-- Wrapper -->
+    <div id="app" class="wrapper">
 
-            <!-- main pages -->
-            @yield('content')
+        <!-- navbar temp -->
+        @include('layouts.main.navbar')
 
-            
-        </div> 
+        <!-- main pages -->
+        @include('layouts.main.main')
 
-        <!-- script -->
-        @include('layouts.shared.script')
-    </body>
+    </div>
+
+    <!-- script -->
+    @include('layouts.shared.script')
+</body>
+
 </html>
--- resources/views/layouts/shared/script.php
+++ resources/views/layouts/shared/script.php
@@ -1,9 +1,8 @@
-<!-- Bootstrap 5 -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<!-- Bootstrap 5 bundle-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
+crossorigin="anonymous"></script>
 
-<!-- sidebar script -->
-<script src="{{ asset('dist/js/jquery.min.js') }}"></script>
-<script src="{{ asset('dist/js/popper.js') }}"></script>
-<script src="{{ assets('dist/js/bootstrap.min.js') }}"></script>
-<script src="{{ assets('dist/js/main.js') }}"></script>
+
+
 
